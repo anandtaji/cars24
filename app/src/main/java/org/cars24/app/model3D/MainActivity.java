1a6f999f05a3849c778921eb8c508b92b481fe64
Anand <anand.s@ezetap.com>
2019-01-05 19:48:42 +0530

    init

---
+++ app/src/main/java/org/cars24/app/model3D/MainActivity.java
@@ -0,0 +1,75 @@
+package org.cars24.app.model3D;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+
+import org.cars24.app.model3D.view.MenuActivity;
+import org.cars24.app.model3D.view.ModelActivity;
+import org.cars24.util.android.AndroidURLStreamHandlerFactory;
+import org.cars24.dddmodel2.R;
+
+import java.net.URL;
+
+/**
+ * This is the main android activity. From here we launch the whole stuff.
+ * <p>
+ * Basically, this activity may serve to show a Splash screen and copy the assets (obj models) from the jar to external
+ * directory.
+ *
+ * @author andresoviedo
+ */
+public class MainActivity extends Activity {
+
+
+    // Custom handler: org/andresoviedo/app/util/url/android/Handler.class
+    static {
+        System.setProperty("java.protocol.handler.pkgs", "org.andresoviedo.util.android");
+        URL.setURLStreamHandlerFactory(new AndroidURLStreamHandlerFactory());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set main layout controls.
+        // Basically, this is a screen with the app name just in the middle of the scree
+        setContentView(R.layout.activity_main);
+
+
+        // Start Model activity.
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    //Display for 3 seconds
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    // TODO: handle exception
+                    e.printStackTrace();
+                } finally {
+                    //Goes to Activity  StartingPoint.java(STARTINGPOINT)
+                    MainActivity.this.startActivity(new Intent(MainActivity.this.getApplicationContext(), MenuActivity.class));
+                    MainActivity.this.finish();
+                }
+            }
+        };
+        timer.start();
+
+
+    }
+
+    @SuppressWarnings("unused")
+    private void init() {
+        MainActivity.this.startActivity(new Intent(MainActivity.this.getApplicationContext(), ModelActivity.class));
+        MainActivity.this.finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
---
+++ engine/src/main/java/org/cars24/util/android/AndroidURLStreamHandlerFactory.java
@@ -0,0 +1,17 @@
+package org.cars24.util.android;
+
+import java.net.URLStreamHandler;
+import java.net.URLStreamHandlerFactory;
+
+public class AndroidURLStreamHandlerFactory implements URLStreamHandlerFactory {
+
+    @Override
+    public URLStreamHandler createURLStreamHandler(String protocol) {
+        if ("assets".equals(protocol)) {
+            return new org.cars24.util.android.assets.Handler();
+        } else if ("content".equals(protocol)){
+            return new org.cars24.util.android.content.Handler();
+        }
+        return null;
+    }
+}
